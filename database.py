888fa1e11af1ef3a14594412d4dba5a0683b2e7d
thanhhatran <marco@reelsights.com>
2023-10-06 00:54:49 +0700

    change to postgres

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+# Update your PostgreSQL database URL
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:12345Rs@localhost:8568/postgres"
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///blog.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# Create the PostgreSQL engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
